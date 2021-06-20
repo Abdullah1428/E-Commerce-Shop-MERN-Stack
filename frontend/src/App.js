bebbae4784501658be61de18dbd0aab05bfbfe16
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-20 22:45:04 +0500

    added bootswatch theme lux file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Pro Shop</h1>
+      <Header />
+      <main>
+        <h1>Welcome to Pro Shop</h1>
+      </main>
+      <Footer />
     </>
   )
 }

277dfdb6fd005ab46867032f940ad52de090b322
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-25 14:57:59 +0500

    added colors for consoles

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+
+// connect to MongoDB
 import connectDB from './config/db.js'
+
+// dummy products
 import products from './data/products.js'
 
 dotenv.config()
@@ -27,5 +32,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )

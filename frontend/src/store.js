30d6b13ceffe47209f4249d329de232292498609
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-25 23:15:09 +0500

    added reducer and action for getting single product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './redux/reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer
+} from './redux/reducers/productReducers'
 
 const reducer = combineReducers({
-  productList: productListReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer
 })
 
 const initialState = {}

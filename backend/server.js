d5454bf4d2c14f6837142a18ffc68024e5d8ad78
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-26 11:57:07 +0500

    added basic authentication for user

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,29 @@ import colors from 'colors'
 
 // error middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-
 // connect to MongoDB
 import connectDB from './config/db.js'
 // route for product
 import productRoutes from './routes/productRoutes.js'
+// route for user and auth
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
-
+// connect to DB
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000

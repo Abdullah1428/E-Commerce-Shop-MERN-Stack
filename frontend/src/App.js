a3c8567eb58b7cc551fab8be1686d9058fe4012c
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-26 13:34:31 +0500

    added frontend screen for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+
+// components
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+// screens
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const App = () => {
   return (
@@ -14,9 +18,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />

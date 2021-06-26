ce41edbfcd838125e68e59081ad336a5ac2add8d
Abdullah1428 <abdullahcse1428@gmail.com>
2021-06-26 14:43:00 +0500

    added dropdown in nav and logout function

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -40,3 +40,11 @@ export const login = (email, password) => async dispatch => {
     })
   }
 }
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('userInfo')
+
+  dispatch({
+    type: USER_LOGOUT
+  })
+}
